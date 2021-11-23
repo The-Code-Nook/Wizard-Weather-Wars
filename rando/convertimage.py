f99759fed60bfbc1fab8afe411fb5a239bdf24e5
aaguy-hue <62529127+aaguy-hue@users.noreply.github.com>
2021-11-23 13:05:46 -0800

    weather message doesn't stack

--- rando/convertimage.py
+++ rando/convertimage.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 
 # Rotating and resizing
 # new = Image.open("assets\\images\\firesword_original.png").resize((8, 45), Image.ANTIALIAS).rotate(-10, resample=Image.BICUBIC, expand=True)
@@ -18,9 +18,18 @@ from PIL import Image
 #     rotation += 1
 # imgs[0].save("assets\\images\\firesword_attacking.gif", save_all=True, append_images=imgs[1:], optimize=False, duration=40, loop=0)
 
-rotation = -10
-imgs = [Image.open("assets\\images\\icesword.png")]
-while rotation <= 12:
-    imgs.append(imgs[-1].rotate(rotation, resample=Image.BICUBIC, expand=True))
-    rotation += 1
-imgs[0].save("assets\\images\\icesword_attacking.gif", save_all=True, append_images=imgs[1:], optimize=False, duration=40, loop=0)
+# rotation = -10
+# imgs = [Image.open("assets\\images\\icesword.png")]
+# while rotation <= 12:
+#     imgs.append(imgs[-1].rotate(rotation, resample=Image.BICUBIC, expand=True))
+#     rotation += 1
+# imgs[0].save("assets\\images\\icesword_attacking.gif", save_all=True, append_images=imgs[1:], optimize=False, duration=40, loop=0)
+
+img = Image.open("assets\\images\\firesword.png")
+img2 = Image.open("assets\\images\\icesword.png")
+
+flipped = ImageOps.mirror(img)
+flipped2 = ImageOps.mirror(img2)
+
+flipped.save("assets\\images\\firesword_flipped.png")
+flipped2.save("assets\\images\\icesword_flipped.png")
