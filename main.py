ea3ea73629ae20e28f336fe0b0ee0dc0a7fdedc5
aaguy-hue <62529127+aaguy-hue@users.noreply.github.com>
2021-11-25 10:21:36 -0800

    fixed respawning making multiple game loops, fixed issues near border

--- main.py
+++ main.py
@@ -28,16 +28,15 @@ class Tile:
     def __init__(self, canvas, x1, y1, x2, y2, color):
         self.canvas = canvas
         self.id = self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
-        #self.canvas.move(self.id, 245, 100)
 
 
 class HealthBar:
-    def __init__(self, canvas, startingposX, startingposY):
-        self.canvas = canvas
+    def __init__(self, canvas: Canvas, startingposX: int, startingposY: int):
+        self.canvas: Canvas = canvas
         self.fillid = self.canvas.create_rectangle(0, 0, 0, 10, fill="#FF0000")
         self.outline = self.canvas.create_rectangle(0, 0, 100, 10)
-        self.startingposX = startingposX
-        self.startingposY = startingposY
+        self.startingposX: int = startingposX
+        self.startingposY: int = startingposY
         self.canvas.move(self.fillid, self.startingposX, self.startingposY)
         self.canvas.move(self.outline, self.startingposX, self.startingposY)
     
@@ -47,6 +46,10 @@ class HealthBar:
         x1 = self.startingposX + newhealth
         # x1 = newhealth
         self.canvas.coords(self.fillid, x0, y0, x1, y1)
+    
+    def delete(self):
+        self.canvas.delete(self.fillid)
+        self.canvas.delete(self.outline)
 
 
 class Weapon:
@@ -107,16 +110,12 @@ class Weapon:
                 self.canvas.move(self.id, playercoords[2] - coords[0]-35, playercoords[3] - coords[1]-80)
         
     def face_left(self):
-        # make sure we're not already facing left
-        if not self.facing: return
         self.attacking_frame = 0
         self.canvas.delete(self.id)
         self.id = self.canvas.create_image((0,0), image=self.file_rotate)
         self.attacking = False
 
     def face_right(self):
-        # make sure we're not already facing right
-        if self.facing: return
         self.attacking_frame = 0
         self.canvas.delete(self.id)
         self.id = self.canvas.create_image((0,0), image=self.file)
@@ -126,6 +125,10 @@ class Weapon:
         if not self.attacking:
             self.attacking_frame = 1
             self.attacking = True
+    
+    def delete(self):
+        self.canvas.delete(self.id)
+        self.canvas.delete(self.id)
 
 class Environment:
     def __init__(self, canvas) -> None:
@@ -156,12 +159,13 @@ class Environment:
         self.loadingtext1 = canvas.create_text(600, 200, text="Environment is now COLD!", font="Comic_Sans 20 italic bold", fill="blue")
     
     def clear(self):
+        self.canvas.configure(bg="skyblue")
         self.canvas.delete(self.loadingtext1)
         self.canvas.delete(self.id)
         self.canvas.delete(self.id2)
 
 class Player:
-    def __init__(self, canvas: Canvas, Up, Left, Right, Attack, color: str, startingposX, startingposY, weapon: Weapon, healthbar: HealthBar, name: str, Power, isHot, facing, weakness, env: Environment, sprite, sprite_reverse):
+    def __init__(self, canvas: Canvas, Up, Left, Right, Attack, color: str, startingposX, startingposY, name: str, Power, isHot, facing, weakness, env: Environment, sprite, sprite_reverse, weapon: Weapon=None, healthbar: HealthBar=None):
         self.canvas: Canvas = canvas
         self.color: str = color
         self.Attack = Attack
@@ -176,13 +180,13 @@ class Player:
         self.file = ImageTk.PhotoImage(self.img)
         self.file_reverse = ImageTk.PhotoImage(self.img_reverse)
         self.canvas.move(self.id, startingposX, startingposY)
-        self.acceleration_y = 0.6
+        self.acceleration_y: float = 0.6
         self.velocity_y = 0
         self.velocity_x = 0
-        self.jump_count = 0
-        self.left_stop = True
-        self.right_stop = True
-        self.isHot = isHot
+        self.jump_count: int = 0
+        self.left_stop: bool = True
+        self.right_stop: bool = True
+        self.isHot: bool = isHot
         self.name: str = name
         # player inertia is friction
         self.player_inertia = 0.08
@@ -195,6 +199,8 @@ class Player:
         self.sprint_velocity = 7
         self.jump_height = 12
         self.dead = False
+        self.nametag = self.canvas.create_text(startingposX+8, startingposY-25, font="Times 10", text=self.name)
+        self.hitboxes_enabled = False
 
         if facing == "right":
             self.sprite = self.canvas.create_image((startingposX + 10, startingposY + 48), image=self.file)
@@ -204,7 +210,18 @@ class Player:
             self.facing = False
         else:
             raise ValueError(f"Player facing attribute can either be, 'left', or 'right', not '{facing}'")
+        
+        self.health = 100
+        # This will range from 0.1 to 1.9 depending on the weather
+        self.attack_multiplier = 1
+        self.defense_multiplier = 1
 
+        if self.weapon is not None:
+            self.set_bindings()
+        if self.healthbar is not None:
+            self.healthbar.update()
+
+    def set_bindings(self):
         self.canvas.bind_all(f"<KeyPress-{self.Up}>", self.jump)
         self.canvas.bind_all(f"<KeyPress-{self.Left}>", self.left)
         self.canvas.bind_all(f"<KeyPress-{self.Right}>", self.right)
@@ -212,14 +229,6 @@ class Player:
         self.canvas.bind_all(f"<KeyRelease-{self.Right}>", self.right_stopper)
         self.canvas.bind_all(f"<KeyRelease-{self.Attack}>", self.weapon.attack)
         #self.canvas.bind_all(f"<KeyRelease-{self.Power}>", self.powerUp)
-
-        self.canvas.tag_raise(self.id)
-
-        self.health = 100
-        self.healthbar.update(self.health)
-        # This will range from 0.1 to 1.9 depending on the weather
-        self.attack_multiplier = 1
-        self.defense_multiplier = 1
     
     def left_stopper(self, button):
         self.left_stop = True
@@ -231,9 +240,13 @@ class Player:
         if self.left_stop and self.right_stop:
             self.deactivated = True
 
+    def move(self,x,y):
+        self.canvas.move(self.id, x, y)
+        self.canvas.move(self.sprite, x, y)
+        self.canvas.move(self.nametag, x, y)
+
     def draw(self):
-        self.canvas.move(self.id, self.velocity_x, self.velocity_y)
-        self.canvas.move(self.sprite, self.velocity_x, self.velocity_y)
+        self.move(self.velocity_x, self.velocity_y)
 
         self.velocity_y += self.acceleration_y
         coords = canvas.coords(self.id)
@@ -241,22 +254,18 @@ class Player:
         # Check if we've hit the ground
         if coords[3] > 680:
             self.velocity_y = 0
-            self.canvas.move(self.id, 0, 680-coords[3])
-            self.canvas.move(self.sprite, 0, 680-coords[3])
+            self.move(0, 680-coords[3])
             self.jump_count = 2
             self.player_inertia = 0.8
         
         # Check if we're at the left border
         if coords[0] <= 5:
             self.velocity_x = 0
-            self.canvas.move(self.id, 5-coords[0], 0)
-            self.canvas.move(self.sprite, 5-coords[0], 0)
+            self.move(5-coords[0], 0)
         # Check if we're at the right border
         if coords[2] >= 1270:
             self.velocity_x = 0
-            self.canvas.move(self.id, 1270-coords[2], 0)
-            self.canvas.move(self.sprite, 1270-coords[2], 0)
-        
+            self.move(1270-coords[2], 0)        
         
         self.weapon.draw(canvas.coords(self.id), self.facing)
         
@@ -304,12 +313,14 @@ class Player:
             self.die()
     
     def die(self):
+        global wintext
+
         self.dead = True
-        self.canvas.create_text(600,320,fill=self.enemy.color,font="Times 50 bold",text=f"{self.enemy.name} wins!")
-        global isdone
-        isdone = True
-        tk.after(500,startgame)
-    
+        wintext = self.canvas.create_text(600,320,fill=self.enemy.color,font="Times 50 bold",text=f"{self.enemy.name} wins!")
+        # Initialize players back to their starting positions
+        tk.after(500, initialize_game)
+        env.clear()
+
     def jump(self, button):
         if self.jump_count:
             self.velocity_y = -self.jump_height
@@ -340,6 +351,19 @@ class Player:
             self.canvas.delete(self.sprite)
             self.sprite = self.canvas.create_image((coords[0], coords[1]), image=self.file)
             self.weapon.face_right()
+    
+    def delete(self):
+        self.canvas.delete(self.id)
+        self.canvas.delete(self.sprite)
+        self.canvas.delete(self.nametag)
+    
+    def toggle_hitboxes(self):
+        if self.hitboxes_enabled:
+            self.hitboxes_enabled = False
+            self.canvas.itemconfigure(self.id, outline="")
+        else:
+            self.hitboxes_enabled = True
+            self.canvas.itemconfigure(self.id, outline="red")
 
 
 exited = False
@@ -350,52 +374,89 @@ def on_quit():
 
 tk.protocol("WM_DELETE_WINDOW", on_quit)
 
-def startgame():    
-    global env
-    
-    isdone = False
-    canvas.delete("all")
-    canvas.configure(bg="skyblue")
+player1 = {
+    "healthbar": None,
+    "weapon": None,
+    "player": None
+}
 
-    canvas.create_text(600, 50, fill="darkblue", font="Comic_Sans 40 italic bold",
-                       text="WIZARD WEATHER WARS")
+player2 = {
+    "healthbar": None,
+    "weapon": None,
+    "player": None
+}
+
+def initialize_game(gamestart=False):
+    global player1
+    global player2
+    global env
+    global canvas
+    
+    if not gamestart:
+        global wintext
+        canvas.delete(wintext)
+
+        player1["healthbar"].delete()
+        player1["weapon"].delete()
+        player1["player"].delete()
+
+        player2["healthbar"].delete()
+        player2["weapon"].delete()
+        player2["player"].delete()
 
     env = Environment(canvas)
+    env.clear()
 
-    ground = Tile(canvas, 0, 720, 1280, 680, "#008000")
+    player1 = {
+        "healthbar": HealthBar(canvas, 0, 50),
+        "weapon": Weapon(canvas,"assets\\images\\icesword.png", "assets\\images\\icesword_rotate.png",  5, True),
+        "player": Player(canvas, "w", "a", "d", "v", "#FF0000", 245, 100, "Player 1", "b", True, 'right', "hot", env, "assets\\images\\icewizard.png", "assets\\images\\icewizard_reverse.png")
+    }
+    player1["healthbar"].update(player1["player"].health)
+    player1["player"].healthbar = player1["healthbar"]
+    player1["player"].weapon = player1["weapon"]
+    player1["player"].set_bindings()
 
-    p1weapon = Weapon(canvas,"assets\\images\\icesword.png", "assets\\images\\icesword_rotate.png",  5, True)
-    p1healthbar = HealthBar(canvas, 0, 50)
-    player1 = Player(canvas, "w", "a", "d", "v", "#FF0000", 245, 100, p1weapon, p1healthbar, "Player 1", "b", True, 'right', "hot", env, "assets\\images\\icewizard.png", "assets\\images\\icewizard_reverse.png")
+    player2 = {
+        "healthbar": HealthBar(canvas, 1180, 50),
+        "weapon": Weapon(canvas, "assets\\images\\firesword.png", "assets\\images\\firesword_rotate.png", 5, False),
+        "player": Player(canvas, "Up", "Left", "Right", "k", "Green", 1035, 100, "Player 2", "l", False, 'left', "cold", env, "assets\\images\\firewizard.png", "assets\\images\\firewizard_reverse.png")
+    }
+    player2["healthbar"].update(player2["player"].health)
+    player2["player"].healthbar = player2["healthbar"]
+    player2["player"].weapon = player2["weapon"]
+    player2["player"].set_bindings()
 
-    p2healthbar = HealthBar(canvas, 1180, 50)
-    p2weapon = Weapon(canvas, "assets\\images\\firesword.png", "assets\\images\\firesword_rotate.png", 5, False)
-    player2 = Player(canvas, "Up", "Left", "Right", "k", "Green", 1035, 100, p2weapon, p2healthbar, "Player 2", "l", False, 'left', "cold", env, "assets\\images\\firewizard.png", "assets\\images\\firewizard_reverse.png")
+    player1["player"].enemy = player2["player"]
+    player2["player"].enemy = player1["player"]
 
-    player1.enemy = player2
-    player2.enemy = player1
+def showdebug(button):
+    # This simply exists because I was noticing weird behavior at the map border, so this was simply to help me by toggling hitboxes
+    global player1
+    global player2
 
-    try:
-        while not isdone and not exited:
-            player1.draw()
+    player1["player"].toggle_hitboxes()
+    player2["player"].toggle_hitboxes()
 
-            player2.draw()
+canvas.bind_all("<KeyPress-F3>", showdebug)
 
-            tk.update_idletasks()
-            tk.update()
-            time.sleep(0.005)
-        else:
-            del ground
+canvas.configure(bg="skyblue")
 
-            del p1weapon
-            del p1healthbar
-            del player1
+canvas.create_text(600, 50, fill="darkblue", font="Comic_Sans 40 italic bold", text="WIZARD WEATHER WARS")
 
-            del p2weapon
-            del p2healthbar
-            del player2
-    except KeyboardInterrupt:
-        pass
+ground = Tile(canvas, 0, 720, 1280, 680, "#008000")
 
-if __name__ == "__main__":
-    startgame()
+initialize_game(gamestart=True)
+
+try:
+    while not exited:
+        player1["player"].draw()
+
+        player2["player"].draw()
+
+        tk.update_idletasks()
+        tk.update()
+        # Swords start flickering if you make the delay less than .015 for some reason
+        time.sleep(0.005)
+except KeyboardInterrupt:
+    pass
